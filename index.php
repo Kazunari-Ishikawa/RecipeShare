a3e200f2aace8131ce4cfe053e95cb9286d06d01
kazunari-ishikawa <zunari.computercity@gmail.com>
2020-01-15 11:26:35 +0900

    Change concepts and Make HTML

--- index.php
+++ index.php
@@ -13,14 +13,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <link rel="stylesheet" href="css/style.css" />
-    <title>みんなのご飯</title>
+    <title>わたしのご飯</title>
   </head>
   <body>
     <!-- ヘッダー -->
     <header>
       <div class="header-container">
-        <div class="header-tiitle">
-          <h1>みんなのご飯</h1>
+        <div class="header-title">
+          <img src="" alt="" />
         </div>
         <nav class="header-nav">
           <ul>
@@ -33,52 +33,56 @@
     </header>
 
     <!-- メイン -->
-    <main>
-      <!-- ABOUT -->
+    <div class="container">
+      <!-- トップ画面 -->
       <section id="ABOUT">
         <div class="about-container">
-          <h2>ABOUT</h2>
+          <h1>わたしのご飯</h1>
           <p>
-            ここはみんなが作ったご飯を共有するサイトです<br />今日のレシピに迷ったらヒントをもらいましょう
+            作ったご飯を登録しておくことで<br />今日のレシピに迷ったときにさっと検索できます
           </p>
         </div>
       </section>
 
-      <!-- サイドバー -->
-      <section id="side-bar">
-        <div class="side-bar">
-          <form action="" method="post">
-            <p>投稿者</p>
-            <input type="text" name="username" value="" />
-            <p>カテゴリ</p>
-            <select name="category" id="">
-              <option value="0">選択してください</option>
-            </select>
-            <input type="submit" value="検索" />
-          </form>
-        </div>
-      </section>
-
-      <!-- メインコンテンツ -->
-      <section id="contents">
-        <div class="contents-container">
-          <div class="panel-container">
-            <div class="panel">
-              <img src="" alt="" />
-              <p>投稿者</p>
-              <p>日付</p>
-              <p>タイトル</p>
+      <!-- コンテンツ -->
+      <section id="DESCRIPTION">
+        <div class="panel-container">
+          <div class="panel description">
+            <div class="panel-header">
+              <h3>写真でかんたん登録</h3>
             </div>
-            <div class="panel">
+            <div class="panel-main">
               <img src="" alt="" />
-              <p>投稿者</p>
-              <p>日付</p>
-              <p>タイトル</p>
+              <div class="panel-txt">
+                <p>写真と料理のカテゴリだけでかんたんに登録できます</p>
+              </div>
+            </div>
+          </div>
+          <div class="panel description">
+            <div class="panel-header">
+              <h3>何を食べたか記録にも</h3>
+            </div>
+            <div class="panel-main">
+              <img src="" alt="" />
+              <div class="panel-txt">
+                <p>料理のカテゴリ、日付などで検索できます</p>
+              </div>
+            </div>
+          </div>
+          <div class="panel description">
+            <div class="panel-header">
+              <h3>忘れたレシピを検索できる</h3>
+            </div>
+            <div class="panel-main">
+              <img src="" alt="" />
+              <div class="panel-txt">
+                <p>レシピ検索ボタンを押すだけでGoogle検索できます</p>
+              </div>
             </div>
           </div>
         </div>
       </section>
-    </main>
+    </div>
 
     <!-- フッター -->
     <footer></footer>
