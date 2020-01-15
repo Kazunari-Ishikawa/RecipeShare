e488215a3b0dbdf583d01337acaf50a47ff343c8
kazunari-ishikawa <zunari.computercity@gmail.com>
2020-01-15 09:21:22 +0900

    make html

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
-<?php 
+<?php
 // 共通関数の読み込み
-require('function.php');
+// require('function.php');
 
-debugLogStart();
+// debugLogStart();
 
 ?>
 
@@ -12,21 +12,77 @@ debugLogStart();
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>Recipe</title>
+    <link rel="stylesheet" href="css/style.css" />
+    <title>みんなのご飯</title>
   </head>
   <body>
+    <!-- ヘッダー -->
     <header>
-      <div class="header-tiitle"></div>
-      <nav class="header-nav">
-        <ul>
-          <li><a href="index.html">トップページ</a></li>
-          <li><a href="#">ユーザー登録</a></li>
-        </ul>
-      </nav>
+      <div class="header-container">
+        <div class="header-tiitle">
+          <h1>みんなのご飯</h1>
+        </div>
+        <nav class="header-nav">
+          <ul>
+            <li><a href="index.html">トップページ</a></li>
+            <li><a href="#">ユーザー登録</a></li>
+            <li><a href="#">ログイン</a></li>
+          </ul>
+        </nav>
+      </div>
     </header>
 
+    <!-- メイン -->
     <main>
-      <div class="about"></div>
+      <!-- ABOUT -->
+      <section id="ABOUT">
+        <div class="about-container">
+          <h2>ABOUT</h2>
+          <p>
+            ここはみんなが作ったご飯を共有するサイトです<br />今日のレシピに迷ったらヒントをもらいましょう
+          </p>
+        </div>
+      </section>
+
+      <!-- サイドバー -->
+      <section id="side-bar">
+        <div class="side-bar">
+          <form action="" method="post">
+            <p>投稿者</p>
+            <input type="text" name="username" value="" />
+            <p>カテゴリ</p>
+            <select name="category" id="">
+              <option value="0">選択してください</option>
+            </select>
+            <input type="submit" value="検索" />
+          </form>
+        </div>
+      </section>
+
+      <!-- メインコンテンツ -->
+      <section id="contents">
+        <div class="contents-container">
+          <div class="panel-container">
+            <div class="panel">
+              <img src="" alt="" />
+              <p>投稿者</p>
+              <p>日付</p>
+              <p>タイトル</p>
+            </div>
+            <div class="panel">
+              <img src="" alt="" />
+              <p>投稿者</p>
+              <p>日付</p>
+              <p>タイトル</p>
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
+
+    <!-- フッター -->
+    <footer></footer>
+
+    <script src="js/vendor/jquery-3.4.1.min.js"></script>
   </body>
 </html>
