569b94052b7630681442ae0997dcc5419125b160
kazunari-ishikawa <zunari.computercity@gmail.com>
2020-01-17 16:05:00 +0900

    コンテンツ登録機能 編集部分を実装

--- registProduct.php
+++ registProduct.php
@@ -3,14 +3,12 @@
 require('function.php');
 
 debug('◇◆◇◆◇◆◇◆◇◆◇◆◇◆◇');
-debug('ご飯登録');
+debug('ご飯登録・編集ページ');
 debug('◇◆◇◆◇◆◇◆◇◆◇◆◇◆◇');
 debugLogStart();
 
 // ログイン認証
 require('auth.php');
-
-$user_id = $_SESSION['user_id'];
 
 // DBからカテゴリデータを取得
 // カテゴリデータ取得関数
@@ -89,6 +87,45 @@ function uploadImg($file, $key) {
   }
 }
 
+// プロダクト情報取得関数
+function getProduct($product_id) {
+  debug('プロダクト情報を取得します');
+  debug('プロダクトID：'.print_r($product_id, true));
+
+  try {
+    // DB接続
+    $dbh = dbConnect();
+    // SQL作成
+    $sql = 'SELECT * FROM Recipe WHERE id = :p_id AND delete_flg = 0';
+    $data = array(':p_id' => $product_id);
+    // クエリ実行
+    $stmt = queryPost($dbh, $sql, $data);
+    // データ取得
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($stmt) {
+      return $result;
+    } else {
+      return false;
+    }
+
+  } catch(Exception $e) {
+    debug('エラー：'.$e->getMessage());
+    $err_msg['common'] = MSG08;
+  }
+}
+//==============================================================================================
+
+debug('GET：'.print_r($_GET, true));
+
+// 新規登録または編集を判別するフラグ（編集=>1 新規登録=>0）
+$edit_flg = (!empty($_GET['p_id'])) ? true : false;
+
+// 編集の場合GETパラメータから料理のIDを取得
+$product_id = (!empty($_GET['p_id'])) ? $_GET['p_id'] : '';
+$dbProductData = getProduct($product_id);
+debug('料理情報：'.print_r($dbProductData,true));
+
 //================================
 // 画面表示処理
 //================================
@@ -104,13 +141,13 @@ if (!empty($_POST)) {
   $subName = $_POST['sub-name'];
   $comment = $_POST['comment'];
   // 画像をアップロードしパスを保存
-  $pic = (!empty($_FILES['pic'])) ? uploadImg($_FILES['pic'], 'pic') : '';
-
-  // 未入力チェック
-  validRequired($mainName, 'main-name');
+  $pic = (!empty($_FILES['pic']['name'])) ? uploadImg($_FILES['pic'], 'pic') : '';
+  // 画像がPOSTされていないが、DBにすでに登録されている場合DBのデータを代入
+  $pic = (empty($pic) && !empty($dbProductData['pic'])) ? $dbProductData['pic'] : $pic;
 
   // 各項目のバリデーション
-  if (empty($err_msg)) {
+  // 新規登録の場合
+  if ($edit_flg == 0) {
     // カテゴリ
     if (!preg_match("/^[0-9]+$/", $category)) {
       global $err_msg;
@@ -118,19 +155,47 @@ if (!empty($_POST)) {
     }
     // 主菜
     validMaxLen($mainName, 'main-name');
+    validRequired($mainName, 'main-name');
     // 副菜
     validMaxLen($subName, 'sub-name');
     // コメント（コメントは最大500文字まで）
     validMaxLen($comment, 'comment', 500);
 
-    // バリデーションを通った場合
-    if (empty($err_msg)) {
-      debug('バリデーションOK');
+  } else {
+    // 編集の場合DBと不一致の場合にバリデーションを行う
+    // カテゴリ
+    if ($category != $dbProductData['category_id']) {
+      if (!preg_match("/^[0-9]+$/", $category)) {
+        global $err_msg;
+        $err_msg['category'] = '不正な値です';
+      }
+    }
+    // 主菜
+    if ($mainName !== $dbProductData['main_name']) {
+      validMaxLen($mainName, 'main-name');
+      validRequired($mainName, 'main-name');
+    }
+    // 副菜
+    if ($mainName !== $dbProductData['sub_name']) {
+      validMaxLen($mainName, 'sub-name');
+    }
+    // コメント
+    if ($comment !== $dbProductData['comment']) {
+      validMaxLen($comment, 'comment', 500);
+    }
+  }
 
-      debug('DBへ登録します');
-      try {
-        // DB接続
-        $dbh = dbConnect();
+  // バリデーションを通った場合
+  if (empty($err_msg)) {
+    debug('バリデーションOK');
+
+    try {
+      // DB接続
+      $dbh = dbConnect();
+
+      // 新規登録の場合
+      if ($edit_flg == 0) {
+        debug('DBへ登録します');
         // SQL作成
         $sql = 'INSERT INTO Recipe(category_id,main_name,sub_name,comment,pic,user_id,created_at) VALUES (:c_id,:main_name,:sub_name,:comment,:pic,:u_id,:created_at)';
         $data = array(
@@ -139,33 +204,47 @@ if (!empty($_POST)) {
           ':sub_name' => $subName,
           ':comment' => $comment,
           ':pic' => $pic,
-          ':u_id' => $user_id,
+          ':u_id' => $_SESSION['user_id'],
           ':created_at' => date('Y-m-d H:i:s'),
         );
-        debug('$data：'.print_r($data,true));
-        // クエリ実行
-        $stmt = queryPost($dbh, $sql, $data);
-        if ($stmt) {
-          debug('登録成功');
-          $_SESSION['suc_msg'] = 'ご飯を登録しました';
-          header("Location:mypage.php");
-        } else {
-          debug('登録失敗');
-          $err_msg['common'] = MSG08;
-        }
+        // 編集の場合
+      } else {
+        debug('DBを更新します');
+        // SQL作成
+        $sql = 'UPDATE Recipe SET category_id = :c_id, main_name = :main_name, sub_name = :sub_name, comment = :comment, pic = :pic WHERE id = :p_id AND user_id = :u_id';
+        $data = array(
+          ':c_id' => $category,
+          ':main_name' => $mainName,
+          ':sub_name' => $subName,
+          ':comment' => $comment,
+          ':pic' => $pic,
+          ':p_id' => $product_id,
+          ':u_id' => $_SESSION['user_id'],
+        );
+      }
 
-      } catch (Exception $e) {
-        debug('エラー：'.$e->getMessage());
+      debug('$data：'.print_r($data,true));
+
+      // クエリ実行
+      $stmt = queryPost($dbh, $sql, $data);
+      if ($stmt) {
+        debug('登録成功');
+        $_SESSION['suc_msg'] = '登録しました';
+        header("Location:mypage.php");
+      } else {
+        debug('登録失敗');
         $err_msg['common'] = MSG08;
       }
 
-    } else {
-      debug('バリデーションNG');
+    } catch (Exception $e) {
+      debug('エラー：'.$e->getMessage());
+      $err_msg['common'] = MSG08;
     }
 
   } else {
     debug('バリデーションNG');
   }
+
 }
 
 if(!empty($err_msg)) debug('エラー：'.print_r($err_msg,true));
